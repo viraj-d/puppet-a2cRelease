90e12b856c2e5f417a7c1da1f55bb56a418607a4
Viraj <viraj.d@advanced-india.com>
2016-01-04 12:20:32 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,8 @@
 
 class a2creleases::site inherits a2creleases {
 
+ File { source_permissions => ignore }
+
  file{"$sitepath":
     ensure  => directory,
     source	=> 'puppet:///modules/a2creleases/A2CApplication',
